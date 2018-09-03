456d7baf7c008509b8a0a8cb016175f1d3938f93
PcJax <javi.castro@gmail.com>
2018-09-03 09:16:18 -0500

    Restoring NetworkLobby support for Mirror

---
+++ Assets/PcJax/Scripts/Networking/Mirror/Messages.cs
@@ -0,0 +1,20 @@
+﻿namespace Mirror
+{
+    class LobbyReadyToBeginMessage : MessageBase
+    {
+        public byte slotId;
+        public bool readyState;
+
+        public override void Deserialize(NetworkReader reader)
+        {
+            slotId = reader.ReadByte();
+            readyState = reader.ReadBoolean();
+        }
+
+        public override void Serialize(NetworkWriter writer)
+        {
+            writer.Write(slotId);
+            writer.Write(readyState);
+        }
+    }
+}
---
+++ Assets/PcJax/Scripts/Networking/Mirror/Utils.cs
@@ -0,0 +1,13 @@
+﻿using Mirror;
+
+namespace PcJax.Networking.Mirror
+{
+    public class Utils
+    {
+        public static bool GetPlayerController(NetworkConnection conn, short playerControllerId, out PlayerController playerController)
+        {
+            playerController = conn.playerControllers.Find(pc => pc.IsValid && pc.playerControllerId == playerControllerId);
+            return playerController != null;
+        }
+    }
+}
